e2965a1229e4ce383ff3423585a6ff660cca98f2
Олександр Сущевських <65895998+Oleksandr2@users.noreply.github.com>
2021-05-02 13:38:26 +0300

    Update my_game.py

--- my_game.py
+++ my_game.py
@@ -10,24 +10,25 @@ class Player:
         self.damage = 1
         self.heal_hit_points = 1
      
-    # Выбор действия для игрока :
-    # 0 - урон в небольшом диапазоне от 18 до 25
-    # 1 - урон в большом диапазоне от 10 до 35
-    # 2 - исцеление в диапазоне от 18 до 25	   
+    # Выбор действия для игрока :   
     def damage_and_heal(self):
         choice_of_action = random.randint(0,2)
-        if choice_of_action == 0:
+        
+	# 0 - урон в небольшом диапазоне от 18 до 25
+	if choice_of_action == 0:
             self.damage = random.randint(18, 25)
             print("Игрок нанес урон в небольшом диапазоне {}".format(self.damage))
             print()
             return self.damage
-            
+         
+	# 1 - урон в большом диапазоне от 10 до 35    
         if choice_of_action == 1:
             self.damage = random.randint(10, 35)
             print("Игрок нанес урон в небольшом диапазоне {}".format(self.damage))
             print()
             return self.damage
-            
+        
+	# 2 - исцеление в диапазоне от 18 до 25
         if choice_of_action == 2:
             self.heal_hit_points = random.randint(18, 25)
             self.hit_points += self.heal_hit_points
@@ -47,25 +48,29 @@ class Computer:
         self.damage = 1
         self.heal_hit_points = 1
      
-    # Выбор действия для компьютера если очки здоровья больше 35:
-    # 0 - урон в небольшом диапазоне от 18 до 25
-    # 1 - урон в большом диапазоне от 10 до 35
-    # 2 - исцеление в диапазоне от 18 до 25   
+   
+
     def damage_and_heal(self):
+	
+	 # Выбор действия для компьютера если очки здоровья больше 35
         if self.hit_points > 35:
             choice_of_action = random.randint(0,2)
+	    
+	    # 0 - урон в небольшом диапазоне от 18 до 25
             if choice_of_action == 0:
                 self.damage = random.randint(18, 25)
                 print("Компьютер нанес урон в небольшом диапазоне {}".format(self.damage))
                 print()
                 return self.damage
-                
+            
+            # 1 - урон в большом диапазоне от 10 до 35
             if choice_of_action == 1:
                 self.damage = random.randint(10, 35)
                 print("Компьютер нанес урон в большом диапазоне {}".format(self.damage))
                 print()
                 return self.damage
-                
+             
+	    # 2 - исцеление в диапазоне от 18 до 25 
             if choice_of_action == 2:
                 self.heal_hit_points = random.randint(18, 25)
                 self.hit_points += self.heal_hit_points
@@ -76,24 +81,25 @@ class Computer:
                 print("Компьютер восстановил хп на +{}".format(self.heal_hit_points))
                 print()
         
-	    # Выбор действия для компьютера если очки здоровья меньше 35:
-        # 0 - урон в небольшом диапазоне от 18 до 25
-        # 1 - урон в большом диапазоне от 10 до 35
-        # 2,3 - исцеление в диапазоне от 18 до 25         
+	# Выбор действия для компьютера если очки здоровья меньше 35        
         else: 
             choice_of_action = random.randint(0,3)
+	    
+	    # 0 - урон в небольшом диапазоне от 18 до 25
             if choice_of_action == 0:
                 self.damage = random.randint(18, 25)
                 print("Компьютер нанес урон в небольшом диапазоне {}".format(self.damage))
                 print()
                 return self.damage
-                
+            
+	    # 1 - урон в большом диапазоне от 10 до 35
             if choice_of_action == 1:
                 self.damage = random.randint(10, 35)
                 print("Компьютер нанес урон в большом диапазоне {}".format(self.damage))
                 print()
                 return self.damage
-                
+            
+	    # 2 - исцеление в диапазоне от 18 до 25 
             if choice_of_action == 2:
                 self.heal_hit_points = random.randint(18, 25)
                 self.hit_points += self.heal_hit_points
@@ -103,7 +109,8 @@ class Computer:
                     self.hit_points = self.max_hit_points
                 print("Компьютер восстановил хп на +{}".format(self.heal_hit_points))
                 print()
-                
+            
+	    # 3 - исцеление в диапазоне от 18 до 25 
             if choice_of_action == 3:
                 self.heal_hit_points = random.randint(18, 25)
                 self.hit_points += self.heal_hit_points
@@ -122,9 +129,7 @@ class Fight:
         self.computer = Computer()
         self.player = Player()
     
-    # Выбор того, кто ходит:
-    # 1 - ход Компьютера
-    # 2 - ход Игрока    
+    # Выбор того, кто ходит
     def choice_forward(self):
         return random.randint(1,2)
     
@@ -137,11 +142,14 @@ class Fight:
             print("Player: {}/{}".format(self.player.hit_points, self.player.max_hit_points))
             print()
             choice = self.choice_forward()
+	    
+	    # 1 - ход Компьютера
             if choice == 1:
                 choice_of_action = self.computer.damage_and_heal()
                 if choice_of_action == 0 or 1:
                     self.player.hit_points -= self.computer.damage
-                    
+            
+	    # 2 - ход Игрока         
             if choice == 2:
                 choice_of_action = self.player.damage_and_heal()
                 if choice_of_action == 0 or 1:
